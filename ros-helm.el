c6c1e8034bc15dff80773fd733ab2a11955853e6
David Landry <davidlandry93@gmail.com>
2016-07-21 14:25:34 -0400

    Improved readability of actions

--- ros-helm.el
+++ ros-helm.el
@@ -9,14 +9,24 @@
 (defvar ros-helm--launchfile-candidate-list-cache nil)
 (defvar ros-helm--service-candidate-list-cache nil)
 
+(defun ros-helm/open-file-action (filename)
+  (interactive) (find-file filename))
+
+(defun ros-helm/displayed-real-pair-of-path (fullpath)
+  `(,(file-name-nondirectory (file-name-sans-extension fullpath)) . ,fullpath))
+
 (defun ros-helm/list-files-of-command (command)
-  "Accepts a shell COMMAND that lists files and puts them in a list"
+  "Accepts a shell COMMAND that lists files and outputs
+a list of (displayed . real) candidate name."
   (with-temp-buffer
     (call-process-shell-command command nil t)
     (goto-char (point-min))
     (let (filelist)
       (while (not (eq 0 (count-lines (point) (point-max))))
-        (push (buffer-substring (line-beginning-position) (point)) filelist)
+        (push
+         (let ((fullpath (buffer-substring (line-beginning-position) (point))))
+           (ros-helm/displayed-real-pair-of-path fullpath))
+         filelist)
         (end-of-line 2)) ;; move forward a line then go to the end of it
       filelist)))
 
@@ -45,9 +55,7 @@
 (defvar helm-source-ros-services
   (helm-build-sync-source "Services"
     :candidates (ros-helm/service-candidate-list)
-    :action '(("Open file" . (lambda (srvfile-str)
-                               (interactive)
-                               (find-file srvfile-str))))))
+    :action '(("Open file" . ros-helm/open-file-action))))
 
 (defun ros-helm ()
   (interactive)
