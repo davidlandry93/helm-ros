50deafc6582075fa4ee5975d62b60f94ccebd51e
David Landry <davidlandry93@gmail.com>
2016-07-21 10:45:47 -0400

    Fixed the fetching of the candidate list with helm

--- ros-helm.el
+++ ros-helm.el
@@ -11,27 +11,26 @@
 
 (defun fetch-launchfile-candidate-list ()
   "Fills the `launchfile-candidate-list-cache' variable with the name of the launchfiles we could find, an then returns it."
-  (with-current-buffer "    *temp*"
+  (with-temp-buffer
     (call-process-shell-command
-     "find /opt/ros/indigo -type f -name \"*.launch\"" nil (get-buffer "    *temp*"))
+     "find /opt/ros/indigo -type f -name \"*.launch\"" nil t)
     (goto-char (point-min))
     (let (launchfiles)
       (while (not (eq 0 (count-lines (point) (point-max))))
         (push (buffer-substring (line-beginning-position) (point)) launchfiles)
         (message (buffer-substring (line-beginning-position) (point)))
-        (end-of-line))
-      (setq launchfile-candidate-list-cache launchfiles)
-      (kill-buffer)))
+        (end-of-line 2))
+      (setq launchfile-candidate-list-cache launchfiles)))
   launchfile-candidate-list-cache)
 
 (defun launchfile-candidate-list ()
   (if launchfile-candidate-list-cache
-      (fetch-launchfile-candidate-list)
-    launchfile-candidate-list-cache))
+      launchfile-candidate-list-cache
+    (fetch-launchfile-candidate-list)))
 
 (defvar helm-source-ros-launchfiles
   (helm-build-sync-source "Launchfiles"
-    :candidates (map 'list (lambda (path) (make-symbol path)) (launchfile-candidate-list))
+    :candidates (launchfile-candidate-list)
     :action '(("Open file" . (lambda (launchfile-str)
                              (interactive)
                              (find-file launchfile-str))))))
