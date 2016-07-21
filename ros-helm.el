51ebf5fbf66a1c77df61a92706c785b79cc3d28b
David Landry <davidlandry93@gmail.com>
2016-07-21 11:50:30 -0400

    The search for service files is now sync.

--- ros-helm.el
+++ ros-helm.el
@@ -1,32 +1,28 @@
 
+(defvar ros-helm--package-path
+  (replace-regexp-in-string ":" " " (getenv "ROS_PACKAGE_PATH")))
 
-(defvar helm-source-ros-services
-  (helm-build-async-source "Services"
-    :candidates-process (lambda ()
-                          (start-process-shell-command
-                           "find-services" nil
-                           "find"  "/opt/ros/indigo/" "|" "grep" "\\.srv$"))))
+(defvar ros-helm--launchfile-candidate-list-cache nil)
+(defvar ros-helm--service-candidate-list-cache nil)
 
-(defvar launchfile-candidate-list-cache nil)
-
-(defun fetch-launchfile-candidate-list ()
-  "Fills the `launchfile-candidate-list-cache' variable with the name of the launchfiles we could find, an then returns it."
+(defun ros-helm/list-files-of-command (command)
+  "Accepts a shell COMMAND that lists files and puts them in a list"
   (with-temp-buffer
-    (call-process-shell-command
-     "find /opt/ros/indigo -type f -name \"*.launch\"" nil t)
+    (call-process-shell-command command nil t)
     (goto-char (point-min))
-    (let (launchfiles)
+    (let (filelist)
       (while (not (eq 0 (count-lines (point) (point-max))))
-        (push (buffer-substring (line-beginning-position) (point)) launchfiles)
-        (message (buffer-substring (line-beginning-position) (point)))
-        (end-of-line 2))
-      (setq launchfile-candidate-list-cache launchfiles)))
-  launchfile-candidate-list-cache)
+        (push (buffer-substring (line-beginning-position) (point)) filelist)
+        (end-of-line 2)) ;; move forward a line then go to the end of it
+      filelist)))
 
-(defun launchfile-candidate-list ()
-  (if launchfile-candidate-list-cache
-      launchfile-candidate-list-cache
-    (fetch-launchfile-candidate-list)))
+(defun ros-helm/launchfile-candidate-list ()
+  (if ros-helm--launchfile-candidate-list-cache
+      ros-helm--launchfile-candidate-list-cache
+    (set 'ros-helm--launchfile-candidate-list-cache
+          (ros-helm/list-files-of-command
+           (format "find %s -type f -name \"*.launch\"" ros-helm--package-path)))))
+
 
 (defvar helm-source-ros-launchfiles
   (helm-build-sync-source "Launchfiles"
@@ -35,6 +31,20 @@
                              (interactive)
                              (find-file launchfile-str))))))
 
+(defun ros-helm/service-candidate-list ()
+  (if ros-helm--service-candidate-list-cache
+      ros-helm--service-candidate-list-cache
+    (set 'ros-helm--service-candidate-list-cache
+         (ros-helm/list-files-of-command
+          (format "find %s -type f -name \"*.srv\"" ros-helm--package-path)))))
+
+(defvar helm-source-ros-services
+  (helm-build-sync-source "Services"
+    :candidates (ros-helm/service-candidate-list)
+    :action '(("Open file" . (lambda (srvfile-str)
+                               (interactive)
+                               (find-file srvfile-str))))))
+    
 (defun ros-helm ()
   (interactive)
   (helm :sources '(helm-source-ros-services helm-source-ros-launchfiles)
